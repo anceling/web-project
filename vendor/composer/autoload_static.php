89e910770584c5d81f8581ef4430c6d6f181801e
badc0ffee0ddf00d <badc0ffee0ddf00d@users.noreply.github.com>
2016-05-24 02:24:37 +0200

    test route+twig

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -24,11 +24,22 @@ class ComposerStaticInitfd2e169cc1bb027eba6dd7234f80bcd4
         ),
     );
 
+    public static $prefixesPsr0 = array (
+        'T' => 
+        array (
+            'Twig_' => 
+            array (
+                0 => __DIR__ . '/..' . '/twig/twig/lib',
+            ),
+        ),
+    );
+
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
             $loader->prefixLengthsPsr4 = ComposerStaticInitfd2e169cc1bb027eba6dd7234f80bcd4::$prefixLengthsPsr4;
             $loader->prefixDirsPsr4 = ComposerStaticInitfd2e169cc1bb027eba6dd7234f80bcd4::$prefixDirsPsr4;
+            $loader->prefixesPsr0 = ComposerStaticInitfd2e169cc1bb027eba6dd7234f80bcd4::$prefixesPsr0;
 
         }, null, ClassLoader::class);
     }
