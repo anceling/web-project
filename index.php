8e989ff66905755ba4b6baaff3d9b328ab674f8a
badc0ffee0ddf00d <chrome@localhost.localdomain>
2016-05-25 13:34:47 +0200

    index & twig

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ require 'vendor/autoload.php';
 
 
 
-$loader = new Twig_Loader_Filesystem('templates/');
+$loader = new Twig_Loader_Filesystem(__DIR__.'/templates');
 $twig = new Twig_Environment($loader, array(
     // Uncomment the line below to cache compiled templates
     // 'cache' => __DIR__.'/../cache',
