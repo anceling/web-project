60381d584771df39990009a47b2c6df7828f48b5
badc0ffee0ddf00d <chrome@localhost.localdomain>
2016-05-25 13:54:00 +0200

    index & twig

--- index.php
+++ index.php
@@ -62,7 +62,19 @@ switch ($routeInfo[0]) {
 
 
 
-function showindex($vars,$twig){
+function showindex($vars){
+	
+	
+	
+$loader = new Twig_Loader_Filesystem(__DIR__.'/templates');
+$twig = new Twig_Environment($loader, array(
+    // Uncomment the line below to cache compiled templates
+    // 'cache' => __DIR__.'/../cache',
+));
+	
+	
+	
+	
 
 	$name = "Mercury, Venus, Mars, Jupiter, Saturn, Uranus & Neptune.";
 
